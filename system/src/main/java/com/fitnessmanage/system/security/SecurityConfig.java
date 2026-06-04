dde6404071129f1838e6537fddc4c45998ae00ee
Nikita Rudolf <nikitosr423@gmail.com>
2026-06-04 18:57:46 +0300

    chore

--- system/src/main/java/com/fitnessmanage/system/security/SecurityConfig.java
+++ system/src/main/java/com/fitnessmanage/system/security/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
-                                "/error" // КРИТИЧЕСКИ ВАЖНО для Spring Boot 3
+                                "/error"
                         ).permitAll()
 
                         .requestMatchers("/api/v1/auth/**").permitAll()
